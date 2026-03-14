e389b5cf71ca3ff4c472970064d085a9d8230e19
edwardchalstrey1 <edwardchalstrey@gmail.com>
2026-03-14 02:14:55 +0000

    feat: Update map base layer to Carto Voyager and refine responsive layout and typography for timeline, year overlay, and info box.

--- src/components/MapViewer.jsx
+++ src/components/MapViewer.jsx
@@ -34,18 +34,18 @@ const MapViewer = ({ year, onLoaded, setVisiblePolities, onPolitySelect, selecte
       style: {
         version: 8,
         sources: {
-          'carto-positron': {
+          'carto-voyager': {
             type: 'raster',
-            tiles: ['https://a.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png'],
+            tiles: ['https://a.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png'],
             tileSize: 256,
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
           }
         },
         layers: [
           {
-            id: 'carto-positron-layer',
+            id: 'carto-voyager-layer',
             type: 'raster',
-            source: 'carto-positron',
+            source: 'carto-voyager',
             minzoom: 0,
             maxzoom: 20
           }
